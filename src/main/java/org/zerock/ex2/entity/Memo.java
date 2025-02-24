8996b82919eef492db853fd15b7e5a9e192eaf47
woo-be <thsckddn100@gmail.com>
2025-02-24 22:37:11 +0900

    Day 4

---
+++ src/main/java/org/zerock/ex2/entity/Memo.java
@@ -0,0 +1,30 @@
+package org.zerock.ex2.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name= "tbl_memo")
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+}
