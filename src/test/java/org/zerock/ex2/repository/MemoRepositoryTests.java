61de20c1e3cdb9a9472fafd4cf377f54ff07fdc8
woo-be <thsckddn100@gmail.com>
2025-02-26 22:46:21 +0900

    Day 6

--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.ex2.entity.Memo;
 
 @SpringBootTest
@@ -64,5 +65,28 @@ public class MemoRepositoryTests {
         Pageable pageable = PageRequest.of(0,10);
         Page<Memo> result = memoRepository.findAll(pageable);
         System.out.println(result);
+
+        System.out.println("------------------------------------------");
+
+        System.out.println("Total Pages: " + result.getTotalPages());
+        System.out.println("Total Count: " + result.getTotalElements());
+        System.out.println("Page Number: " + result.getNumber());
+        System.out.println("Page Size: " + result.getSize());
+        System.out.println("has nest page?: " + result.hasNext());
+        System.out.println("first page?: " + result.isFirst());
+
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort() {
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sortAll = sort1.and(sort2);
+        Pageable pageable = PageRequest.of(0, 10, sortAll);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        result.get().forEach(System.out::println);
     }
 }
