86b481497f9756c7cf33646f3b4f350e071f257d
woo-be <thsckddn100@gmail.com>
2025-03-11 22:43:06 +0900

    Day 19

--- src/main/java/org/zerock/guestbook/dto/PageResultDTO.java
+++ src/main/java/org/zerock/guestbook/dto/PageResultDTO.java
@@ -8,6 +8,13 @@ import lombok.Data;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+/**
+ * 페이지 결과 화면 구성에 필요한 데이터를 담는 DTO 클래스
+ * @param <DTO>
+ *     DTO
+ * @param <EN>
+ *     Entity
+ */
 @Data
 public class PageResultDTO<DTO, EN> {
 
@@ -15,10 +22,18 @@ public class PageResultDTO<DTO, EN> {
 
     private int totalPage;
 
+    /**
+     * 현재 페이지 번호
+     */
     private int page;
 
     private int size;
 
+    /**
+     * 시작 페이지, 끝 페이지
+     * 화면에 보여지는 시작 페이지 번호와 끝 페이지 번호
+     * 만약 한 화면에 10페이지씩 보여진다면, start=1, end=10 데이터가 부족하면 start=1, end=5
+     */
     private int start, end;
 
     private boolean prev, next;
