1a81ebbfd7e84bdacca6fd0b29e9c69d0148729d
woo-be <thsckddn100@gmail.com>
2025-03-20 23:10:26 +0900

    Day 28

---
+++ src/main/java/org/zerock/board/dto/PageRequestDTO.java
@@ -0,0 +1,32 @@
+package org.zerock.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+/**
+ * 페이징 처리 요청에 필요한 정보를 담는 DTO 클래스
+ * <br>
+ * page, size, type, keyword
+ */
+@Builder
+@AllArgsConstructor
+@Data
+public class PageRequestDTO {
+
+    private int page;
+    private int size;
+    private String type;
+    private String keyword;
+
+    public PageRequestDTO() {
+        this.page = 1;
+        this.size = 10;
+    }
+
+    public Pageable getPageable(Sort sort) {
+        return org.springframework.data.domain.PageRequest.of(page - 1, size, sort);
+    }
+}
--- src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -85,4 +85,14 @@ public class BoardRepositoryTests {
             System.out.println(Arrays.toString(arr));
         });
     }
+
+    @Test
+    public void testRead3() {
+
+        Object result = boardRepository.getBoardByBno(100L);
+
+        Object[] arr = (Object[])result;
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
