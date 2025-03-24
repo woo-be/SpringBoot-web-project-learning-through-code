3054dd46211e8eeb547c9fb38d5f12bb49d8ec3d
woo-be <thsckddn100@gmail.com>
2025-03-24 22:08:54 +0900

    Day 30

--- src/main/java/org/zerock/board/repository/BoardRepository.java
+++ src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.zerock.board.entity.Board;
+import org.zerock.board.repository.search.SearchBoardRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, SearchBoardRepository {
 
     @Query("select b, w from Board b left join b.writer w where b.bno = :bno")
     Object getBoardWithWriter(@Param("bno") Long bno);
