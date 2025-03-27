b3f755427998b18a9c4fa51b78861a17b115f656
woo-be <thsckddn100@gmail.com>
2025-03-27 22:12:17 +0900

    Day 32

--- src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -1,9 +1,15 @@
 package org.zerock.board.repository.search;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.QBoard;
@@ -41,6 +47,70 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
         log.info(result);
 
         return null;
+    }
+
+    @Override
+    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
+
+        log.info("searchPage..........");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        // select b, w, count(r) from Board b
+        // left join b.writer w
+        // left join Reply r on r.board = b
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count());
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        BooleanExpression expression = board.bno.gt(0L);
+
+        booleanBuilder.and(expression);
+
+        if (type != null) {
+            String[] typeArr = type.split("");
+            // 검색 조건을 작성하기
+            BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+            for (String t : typeArr) {
+                switch (t) {
+                    case "t":
+                        conditionBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        conditionBuilder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        conditionBuilder.or(member.email.contains(keyword));
+                        break;
+                }
+            }
+            booleanBuilder.and(conditionBuilder);
+        }
+
+        tuple.where(booleanBuilder);
+
+        tuple.groupBy(board);
+
+        this.getQuerydsl().applyPagination(pageable, tuple);
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
+
+        long count = tuple.fetchCount();
+
+        log.info("COUNT: " + count);
+
+        return new PageImpl<Object[]>(
+                result.stream().map(Tuple::toArray).collect(Collectors.toList()),
+                pageable,
+                count);
     }
 
 }
