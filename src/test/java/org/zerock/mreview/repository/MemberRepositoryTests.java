d5a13ba00526aeb2c40b92b7314a0871fc525e6f
woo-be <thsckddn100@gmail.com>
2025-04-04 22:10:39 +0900

    Day 39

---
+++ src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
@@ -0,0 +1,28 @@
+package org.zerock.mreview.repository;
+
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mreview.entity.Member;
+
+@SpringBootTest
+public class MemberRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void insertMembers() {
+
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Member member = Member.builder()
+                .email("r" + i + "@zerock.org")
+                .pw("1111")
+                .nickname("reviewer" + i)
+                .build();
+            memberRepository.save(member);
+        });
+    }
+
+}
