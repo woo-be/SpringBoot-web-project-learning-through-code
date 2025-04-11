46028db862e7a056fb3917deca3887378ed07702
woo-be <thsckddn100@gmail.com>
2025-04-11 22:42:58 +0900

    Day 45

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -1,6 +1,13 @@
 package org.zerock.mreview.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,14 +16,52 @@ import org.springframework.web.multipart.MultipartFile;
 @Log4j2
 public class UploadController {
 
+    @Value("${org.zerock.upload.path}")
+    private String uploadPath;
+
     @PostMapping("/uploadAjax")
     public void uploadFile(MultipartFile[] uploadFiles) {
 
         for (MultipartFile uploadFile : uploadFiles) {
+
+            if (uploadFile.getContentType().startsWith("image") == false) {
+                log.warn("this file is not image type");
+                return;
+            }
+
             String originalName = uploadFile.getOriginalFilename();
             String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
 
             log.info("fileName: " + fileName);
+
+            String folderPath = makeFolder();
+
+            String uuid = UUID.randomUUID().toString();
+
+            String saveName = uploadPath + File.separator + folderPath + File.separator +
+                uuid + "_" + fileName;
+
+            Path savePath = Path.of(saveName);
+
+            try {
+                uploadFile.transferTo(savePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private String makeFolder() {
+
+        String str = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if (uploadPathFolder.exists() == false) {
+            uploadPathFolder.mkdirs();
+        }
+        return folderPath;
     }
 }
