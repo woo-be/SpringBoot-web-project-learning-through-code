bc9c2006d43437572c871472ca1bdfe2acfa1fc2
woo-be <thsckddn100@gmail.com>
2025-04-17 21:21:24 +0900

    Day 49

---
+++ src/main/java/org/zerock/mreview/dto/MovieDTO.java
@@ -0,0 +1,22 @@
+package org.zerock.mreview.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieDTO {
+
+    private Long mno;
+
+    private String title;
+    
+    @Builder.Default
+    private List<MovieImageDTO> imageDTOList = new ArrayList<>();
+}
