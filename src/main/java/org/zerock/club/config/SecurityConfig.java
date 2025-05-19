50d90c3dc94664f91e4c0fbf5e0ab4d3398e8bfd
woo-be <thsckddn100@gmail.com>
2025-05-19 23:40:59 +0900

    Day 64

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -8,11 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,14 +27,16 @@ public class SecurityConfig {
 
         log.info("----------------------filterChain----------------------");
 
-        http.authorizeHttpRequests((auth) -> {
-            auth.requestMatchers("/sample/all", "/error").permitAll()
-                .requestMatchers("/sample/member").hasRole("USER");
-        });
+        http.authorizeHttpRequests((auth) ->
+            auth.requestMatchers("/sample/all", "/error", "/favicon.ico").permitAll()
+                .requestMatchers("/sample/member").hasRole("USER")
+        );
 
         http.formLogin(withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
         http.logout(withDefaults());
+
+        http.oauth2Login(withDefaults());
 
         return http.build();
     }
